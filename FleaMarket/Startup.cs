6c8abb8551ae21477b30bcf0ef25d2576c3a59b5
KirillCS <KirillCS@users.noreply.github.com>
2021-02-01 13:12:05 +0300

    view was prepared

--- FleaMarket/Startup.cs
+++ FleaMarket/Startup.cs
@@ -20,13 +20,11 @@ namespace FleaMarket
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +34,6 @@ namespace FleaMarket
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
