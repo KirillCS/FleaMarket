236c9d88d9493a850209f9b4ae86d8acd2368687
KirillCS <KirillCS@users.noreply.github.com>
2021-02-01 11:16:23 +0300

    Add project files.

---
+++ FleaMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FleaMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
