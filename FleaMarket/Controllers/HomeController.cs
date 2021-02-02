fd3be9b8e9f30659aa33dc7104e7a2a61461937f
KirillCS <KirillCS@users.noreply.github.com>
2021-02-02 10:43:46 +0300

    items output was added

--- FleaMarket/Controllers/HomeController.cs
+++ FleaMarket/Controllers/HomeController.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using FleaMarket.Models;
 using FleaMarket.ViewModels;
+using FleaMarket.Data;
 
 namespace FleaMarket.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> logger;
+        private readonly DatabaseContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(DatabaseContext context)
         {
-            this.logger = logger;
+            this.context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = new HomeViewModel(this.context.Items);
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ FleaMarket/ViewModels/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FleaMarket.Models;
+using System.Collections.Generic;
+
+namespace FleaMarket.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Item> Items { get; set; }
+
+        public HomeViewModel()
+        {
+
+        }
+
+        public HomeViewModel(IEnumerable<Item> items)
+        {
+            this.Items = items;
+        }
+    }
+}
