41d8e325bea38c4e46409de785d0e53eaae2e8c7
KirillCS <KirillCS@users.noreply.github.com>
2021-02-03 18:28:33 +0300

    view was added

---
+++ FleaMarket/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FleaMarket.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleaMarket.Controllers
+{
+    [Authorize]
+    public class ItemController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddingItemViewModel model)
+        {
+            return Redirect("/");
+        }
+    }
+}
---
+++ FleaMarket/ViewModels/AddingItemViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FleaMarket.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FleaMarket.ViewModels
+{
+    public class AddingItemViewModel
+    {
+        [Required]
+        [MaxLength(256)]
+        [Display(Name = "Name", Prompt = "For example: Smartphone \"Apple\"")]
+        public string Name { get; set; }
+
+        [Required]
+        public IEnumerable<int> CategoriesIds { get; set; }
+
+        [MaxLength(4096)]
+        public string Description { get; set; }
+
+        public IEnumerable<IFormFile> Files { get; set; }
+
+        public int? CoverNumber { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price", Prompt = "For example: 299.99")]
+        [DisplayFormat(ApplyFormatInEditMode = true)]
+        public decimal? Price { get; set; }
+
+        [Display(Name = "Exchange is possible")]
+        public bool TradeEnabled { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
+        public AddingItemViewModel()
+        {
+            Categories = new List<Category>();
+        }
+    }
+}
