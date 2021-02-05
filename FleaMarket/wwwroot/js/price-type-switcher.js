1d666cf191e9fdc146180d36b148ed46b0aa17a7
KirillCS <KirillCS@users.noreply.github.com>
2021-02-05 11:02:04 +0300

    adding price was added

---
+++ FleaMarket/wwwroot/js/price-type-switcher.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(() => {
+    $('.js-price-form-group').each(function () {
+        let form = $(this);
+        $('#pricetype-price', this).change(function () {
+            $('.js-price-input', form)
+                .prop('type', 'number')
+                .val('');
+        });
+
+        $('#pricetype-contract', this).change(function () {
+            $('.js-price-input', form)
+                .prop('type', 'hidden')
+                .val('');
+        });
+
+        $('#pricetype-free', this).change(function () {
+            $('.js-price-input', form)
+                .prop('type', 'hidden')
+                .val('0');
+        });
+    })
+})
