15d46b26d92fb4796de9ee69315747decd9e6405
KirillCS <KirillCS@users.noreply.github.com>
2021-02-07 11:11:23 +0300

    IRepository was added

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace FleaMarket
+{
+    public interface IRepository<TEntity, TId> where TEntity : class
+    {
+        TEntity GetById(TId id);
+
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
