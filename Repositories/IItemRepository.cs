0f936a28e70a9c52c5b48ff30d7e375e1cd0f38d
KirillCS <KirillCS@users.noreply.github.com>
2021-02-07 12:06:46 +0300

    IItemRepository was added

---
+++ Repositories/IItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using FleaMarket.Models;
+using System.Collections.Generic;
+
+namespace FleaMarket
+{
+    public interface IItemRepository : IRepository<Item, int>
+    {
+        IEnumerable<Item> GetAllItemsWithCategories();
+
+        IEnumerable<Image> GetAllCovers();
+
+        Category GetCategoryById(int id);
+
+        Category GetCategoryByCollectionId(IEnumerable<int> ids);
+
+        IEnumerable<Category> GetAllCategories();
+    }
+}
