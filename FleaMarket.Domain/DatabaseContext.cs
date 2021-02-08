61f905a674acaf6fe8af51cf05017df2fa7012bc
KirillCS <KirillCS@users.noreply.github.com>
2021-02-08 19:18:49 +0300

    projects was renamed

---
+++ FleaMarket.Domain/DatabaseContext.cs
@@ -0,0 +1,33 @@
+﻿using FleaMarket.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleaMarket.Domain
+{
+    public class DatabaseContext : IdentityDbContext<User> 
+    {
+        public DbSet<Image> Images { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Item>()
+                   .Property(i => i.TradeEnabled)
+                   .HasDefaultValue(false);
+            builder.Entity<Item>()
+                   .Property(i => i.PublishingDate)
+                   .HasDefaultValueSql("getutcdate()");
+
+            builder.Entity<Image>()
+                   .Property(i => i.IsCover)
+                   .HasDefaultValue(false);
+        }
+    }
+}
